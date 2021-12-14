a2a9ab86b966b7e860729b6bcf792c7f269381ef
Bishal789 <bishalbidari789@gmail.com>
2021-12-14 16:42:15 +0545

    Register UI ni vayo

--- Gharmai/app/src/main/java/com/example/gharmai/UI/UserRegister.kt
+++ Gharmai/app/src/main/java/com/example/gharmai/UI/UserRegister.kt
@@ -2,11 +2,42 @@ package com.example.gharmai.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.gharmai.R
 
 class UserRegister : AppCompatActivity() {
+
+    private lateinit var GenderSpinner:Spinner
+
+    val GenderType = arrayOf("Male,Female,Other")
+    var gender = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_register)
+
+        GenderSpinner = findViewById(R.id.Genderspinner)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, GenderType)
+        GenderSpinner.adapter = adapter
+        GenderSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    gender= parent?.getItemAtPosition(position).toString()
+
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
     }
 }
